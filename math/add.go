b09e8ba77262a215d2f811f4ff94ddb283fbbbb9
Albin Kroon <albkro94@hotmail.com>
2024-08-02 23:44:43 +0200

    Add godoc comments to the math package

--- math/add.go
+++ math/add.go
@@ -1,5 +1,11 @@
+// Package math provides an interface for mathematical equations
 package math
 
+// Add takes two numbers and returns the sum
+//
+// For more information about [addition]
+//
+// [addition]: https://mathisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
